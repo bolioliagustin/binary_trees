1941793bc5bde72640e630ce41cb6544a8f46fdf
agus_1079 <bolioliagustin@gmail.com>
2021-12-02 16:53:44 -0300

    fix 4 5

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,14 +19,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = value;
+	new_node->left = parent->left;
+	parent->left = new_node;
 	new_node->parent = parent;
-	new_node->left = NULL;
+	new_node->n = value;
 	new_node->right = NULL;
 
-	if (parent->left != NULL)
-		parent->left->parent = new_node;
-	parent->left = new_node;
-
+	if (new_node->left != NULL)
+		new_node->left->parent = new_node;
 	return (new_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,14 +19,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	if (parent->right != NULL)
-		parent->right->parent = new_node;
+	new_node->right = parent->right;
 	parent->right = new_node;
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
 
+	if (new_node->right != NULL)
+		new_node->right->parent = new_node;
 	return (new_node);
 }
